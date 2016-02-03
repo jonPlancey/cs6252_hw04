dec5f1380eebf986235e38a436adc43a5f6b4ee5
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-02 21:11:05 -0500

    # code update to add_category.php to work properly

--- add_category.php
+++ add_category.php
@@ -1,44 +1,36 @@
 <?php
+	
+	// Get the product data
+
+	$category = filter_input(INPUT_POST, 'categoryName', FILTER_VALIDATE_INT);
+	
+	$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+	$code = filter_input(INPUT_POST, 'code');
+	$name = filter_input(INPUT_POST, 'name');
+	$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
 
 
-	echo "hlep";
+	// Validate inputs
+	if ($category == null || $category == false ) {
+		$error = "Invalid product data. Check all fields and try again.";
+		include('error.php');
+	} else {
+		require_once('database.php');
 
+		// Add the product to the database  
+		$query = 'INSERT INTO products
+					 (categoryName)
+				  VALUES
+					 (:categoryName)';
+		$statement = $db->prepare($query);
+		$statement->bindValue(':categoryName', $category);
+		$statement->execute();
+		$statement->closeCursor();
 
-/*
+		// Display the Product List page
+		include('category_list.php');
+	}    
 
-// Get the product data
-$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
-$code = filter_input(INPUT_POST, 'code');
-$name = filter_input(INPUT_POST, 'name');
-$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
-
-// Validate inputs
-if ($category_id == null || $category_id == false ||
-        $code == null || $name == null || $price == null || $price == false) {
-    $error = "Invalid product data. Check all fields and try again.";
-    include('error.php');
-} else {
-    require_once('database.php');
-
-    // Add the product to the database  
-    $query = 'INSERT INTO products
-                 (categoryID, productCode, productName, listPrice)
-              VALUES
-                 (:category_id, :code, :name, :price)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':category_id', $category_id);
-    $statement->bindValue(':code', $code);
-    $statement->bindValue(':name', $name);
-    $statement->bindValue(':price', $price);
-    $statement->execute();
-    $statement->closeCursor();
-
-    // Display the Product List page
-    include('category_list.php');
     
-    */
-    
-    
-    
-}
+
 ?>
